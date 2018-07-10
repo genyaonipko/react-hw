d59296fdfa69871217054c076c65551446c28c80
Yevhenii Onipko <onipko32@gmail.com>
2018-07-10 20:33:16 +0300

    added fixes

--- src/common/Card.jsx
+++ src/common/Card.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import style from './StyleInputs.css';
 
-
-
 const Card = ({ hero, deletehero, inform, addhero, isAdded }) => (
-    <div className={ style.card }>
-        <h2 className={ style.title }>{ hero.name }</h2>
-        <div>
-            {!isAdded ? <button onClick={ addhero } name={ hero.name } className={ style.plus }>&#43;</button> : ''}
-            <button onClick={deletehero} name={ hero.name } className={ style.delete }>&#8416;</button>
-            <button onClick={ inform } name={ hero.name } className={ style.inform }>&#8505;</button>
-        </div>
+  <div className={style.card}>
+    <h2 className={style.title}>{hero.name}</h2>
+    <div>
+      {!isAdded ? (
+        <button onClick={addhero} name={hero.name} className={style.plus}>
+          &#43;
+        </button>
+      ) : (
+        ''
+      )}
+      <button onClick={deletehero} name={hero.name} className={style.delete}>
+        &#8416;
+      </button>
+      <button onClick={inform} name={hero.name} className={style.inform}>
+        &#8505;
+      </button>
     </div>
-) 
-
+  </div>
+);
 
 Card.propTypes = {
-    hero: PropTypes.shape().isRequired,
-    deletehero: PropTypes.func.isRequired,
-    inform: PropTypes.func.isRequired,
-    addhero: PropTypes.func.isRequired,
-    isAdded: PropTypes.bool.isRequired
-}
+  hero: PropTypes.shape().isRequired,
+  deletehero: PropTypes.func.isRequired,
+  inform: PropTypes.func.isRequired,
+  addhero: PropTypes.func.isRequired,
+  isAdded: PropTypes.bool.isRequired,
+};
 
 Card.defaultProps = {
-    isAdded: false,
-    addhero: () => console.log('card')
-}
+  isAdded: false,
+  addhero: () => console.log('card'),
+};
 
-export default Card
+export default Card;
