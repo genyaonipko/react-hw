46ba9f5dbd2c0737ca2f5915c07d6f1e797fa567
Yevhenii Onipko <onipko32@gmail.com>
2018-07-22 15:41:23 +0300

    added fixes in route

--- hw3/src/index.js
+++ hw3/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 import { Provider } from "react-redux";
 import store, { history } from "./redux/store";
@@ -9,7 +10,7 @@ import App from "./App";
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <Route path="/" component={App} />
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
